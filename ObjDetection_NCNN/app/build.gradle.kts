d81651ab89eeeb412c72ad44c6c564e5c7ffc892
Galasnow <854932917@qq.com>
2024-07-23 14:16:44 +0800

    Upgrade to SDK 35, ndk r27, jdk 22 and Kotlin 2.0

--- ObjDetection_NCNN/app/build.gradle.kts
+++ ObjDetection_NCNN/app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.objdetection"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.objdetection.ncnn"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 6
         versionName = "1.0.1"
 
@@ -23,8 +23,7 @@ android {
             }
         }
         ndk {
-            moduleName = "ncnn"
-            abiFilters += listOf("arm64-v8a", "x86_64")
+            abiFilters += listOf("arm64-v8a")
         }
     }
 
@@ -39,11 +38,11 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_21
-        targetCompatibility = JavaVersion.VERSION_21
+        sourceCompatibility = JavaVersion.VERSION_22
+        targetCompatibility = JavaVersion.VERSION_22
     }
     kotlinOptions {
-        jvmTarget = "21"
+        jvmTarget = "22"
     }
     externalNativeBuild {
         cmake {
@@ -55,28 +54,29 @@ android {
     buildFeatures {
         viewBinding = true
     }
-    ndkVersion = "26.1.10909125"
+    ndkVersion = "27.0.12077973"
+    buildToolsVersion = "35.0.0"
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.12.0-alpha02")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference-ktx:1.2.1")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit-ktx:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit-ktx:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     implementation("androidx.exifinterface:exifinterface:1.3.7")
-    implementation("androidx.activity:activity-ktx:1.8.2")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.activity:activity-ktx:1.9.0")
+    implementation("androidx.fragment:fragment-ktx:1.8.1")
 
     ksp("androidx.room:room-compiler:2.6.1")
 
     // CameraX core library using the camera2 implementation
-    val camerax_version = "1.4.0-alpha03"
+    val camerax_version = "1.4.0-beta02"
     // The following line is optional, as the core library is included indirectly by camera-camera2
     implementation("androidx.camera:camera-core:${camerax_version}")
     implementation("androidx.camera:camera-camera2:${camerax_version}")
